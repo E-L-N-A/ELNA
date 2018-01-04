7810e0b17722e9ea398d691d2c3c6bcdc401c100
fllp <fllp@foxmail.com>
2018-01-04 16:18:27 -0500

    Revert "Functionality Begins!"
    This reverts commit 47e9c8dff02d6469ef8e2186adfe9e0b5090e68c.

--- Prototype/Prototype/Form1.cs
+++ Prototype/Prototype/Form1.cs
@@ -12,12 +12,15 @@ using MaterialSkin.Animations;
 using MaterialSkin;
 using System.IO;
 using System.Net.Http;
+<<<<<<< HEAD
 using System.Net;
 using System.Web.Script.Serialization;
 using Newtonsoft.Json.Linq;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
 using System.Diagnostics;
+=======
+>>>>>>> parent of 47e9c8d... Functionality Begins!
 
 namespace Prototype
 {
@@ -43,6 +46,7 @@ namespace Prototype
             //richTextBox1.Text = lines;
 
         }
+<<<<<<< HEAD
         //------------------------------------------------------------------------------------------------------------------------------------------------------
         //------------------------------------------------------------------------------------------------------------------------------------------------------
         //------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -172,53 +176,14 @@ namespace Prototype
         //------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+=======
+>>>>>>> parent of 47e9c8d... Functionality Begins!
 
 
         private void materialRaisedButton1_Click(object sender, EventArgs e)
         {
-            if (metroComboBox1.SelectedIndex == 1)
-            {
-                if (metroComboBox3.SelectedIndex == 0)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "en");
-                    Output.Text = translation;
-                }
-                if (metroComboBox3.SelectedIndex == 1)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "zh");
-                    string definition = Youdao_Dictionary(User_Text.Text);
-                    Output.Text = translation+"\r\n"+definition;
-                }
-                if (metroComboBox3.SelectedIndex == 2)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "es");
-                    Output.Text = translation;
-                }
-                if (metroComboBox3.SelectedIndex == 3)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "fr");
-                    Output.Text = translation;
-                }
-                if (metroComboBox3.SelectedIndex == 4)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "ru");
-                    Output.Text = translation;
-                }
-                if (metroComboBox3.SelectedIndex == 5)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "ko");
-                    Output.Text = translation;
-                }
-                if (metroComboBox3.SelectedIndex == 6)
-                {
-                    string translation = Google_Translate(User_Text.Text, "auto", "ja");
-                    Output.Text = translation;
-                }
-            }
-            if (metroComboBox1.SelectedIndex == 0)
-            {
-
-            }
+            
+            
         }
 
         private void materialRaisedButton2_Click(object sender, EventArgs e)
@@ -334,6 +299,7 @@ namespace Prototype
         {
 
         }
+<<<<<<< HEAD
 
         private void metroButton1_Click(object sender, EventArgs e)
         {
@@ -375,5 +341,7 @@ namespace Prototype
     public class Page
     {
         public string extract { get; set; }
+=======
+>>>>>>> parent of 47e9c8d... Functionality Begins!
     }
 }
